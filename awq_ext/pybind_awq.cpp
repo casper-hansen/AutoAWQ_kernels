16c5fe16019e26b161d92ef3002826bce2242cc6
Casper <casperbh.96@gmail.com>
2023-12-28 17:04:21 +0100

    Add dequantization kernel

--- awq_ext/pybind_awq.cpp
+++ awq_ext/pybind_awq.cpp
@@ -12,4 +12,5 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
     m.def("gemmv2_forward_cuda", &gemmv2_forward_cuda, "Quantized v2 GEMM kernel.");
     m.def("gemv_forward_cuda", &gemv_forward_cuda, "Quantized GEMV kernel.");
     m.def("rotary_embedding_neox", &rotary_embedding_neox, "Apply GPT-NeoX style rotary embedding to query and key");
+    m.def("dequantize_weights_cuda", &dequantize_weights_cuda, "Dequantize weights.");
 }
