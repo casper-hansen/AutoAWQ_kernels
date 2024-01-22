f0502a103db68e692d6424c6b2c9ea3f6b7ddc4e
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2024-01-22 04:58:15 +0000

    added rocm support

--- setup.py
+++ setup.py
@@ -3,21 +3,30 @@ import torch
 from pathlib import Path
 from setuptools import setup, find_packages
 from distutils.sysconfig import get_python_lib
-from torch.utils.cpp_extension import BuildExtension, CUDA_HOME, CUDAExtension
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 os.environ["CC"] = "g++"
 os.environ["CXX"] = "g++"
 AUTOAWQ_KERNELS_VERSION = "0.0.2"
 PYPI_BUILD = os.getenv("PYPI_BUILD", "0") == "1"
+CUDA_VERSION = os.getenv("CUDA_VERSION", None) or torch.version.cuda
+ROCM_VERSION = os.environ.get("ROCM_VERSION", None) or torch.version.hip
+
 
 if not PYPI_BUILD:
-    try:
-        CUDA_VERSION = "".join(
-            os.environ.get("CUDA_VERSION", torch.version.cuda).split(".")
-        )[:3]
+    # only adding CUDA/ROCM version if we are not building for PyPI to comply with PEP 440
+    if CUDA_VERSION:
+        CUDA_VERSION = "".join(CUDA_VERSION.split("."))[:3]
         AUTOAWQ_KERNELS_VERSION += f"+cu{CUDA_VERSION}"
-    except Exception as ex:
-        raise RuntimeError("Your system must have an Nvidia GPU for installing AutoAWQ")
+    elif ROCM_VERSION:
+        ROCM_VERSION = "".join(ROCM_VERSION.split("."))[:2]
+        AUTOAWQ_KERNELS_VERSION += f"+rocm{ROCM_VERSION}"
+    else:
+        raise RuntimeError(
+            "Your system must have either Nvidia or AMD GPU to build this package."
+        )
+
+print(f"Building AutoAWQ Kernels version {AUTOAWQ_KERNELS_VERSION}")
 
 common_setup_kwargs = {
     "version": AUTOAWQ_KERNELS_VERSION,
@@ -54,11 +63,13 @@ requirements = [
 def get_include_dirs():
     include_dirs = []
 
-    conda_cuda_include_dir = os.path.join(
-        get_python_lib(), "nvidia/cuda_runtime/include"
-    )
-    if os.path.isdir(conda_cuda_include_dir):
-        include_dirs.append(conda_cuda_include_dir)
+    if CUDA_VERSION:
+        conda_cuda_include_dir = os.path.join(
+            get_python_lib(), "nvidia/cuda_runtime/include"
+        )
+        if os.path.isdir(conda_cuda_include_dir):
+            include_dirs.append(conda_cuda_include_dir)
+
     this_dir = os.path.dirname(os.path.abspath(__file__))
     include_dirs.append(this_dir)
 
@@ -67,6 +78,8 @@ def get_include_dirs():
 
 def get_generator_flag():
     generator_flag = []
+
+    # if CUDA_VERSION:
     torch_dir = torch.__path__[0]
     if os.path.exists(
         os.path.join(torch_dir, "include", "ATen", "CUDAGeneratorImpl.h")
@@ -76,85 +89,85 @@ def get_generator_flag():
     return generator_flag
 
 
-def check_dependencies():
-    if CUDA_HOME is None:
-        raise RuntimeError(
-            f"Cannot find CUDA_HOME. CUDA must be available to build the package."
-        )
-
-
 def get_compute_capabilities():
-    # Collect the compute capabilities of all available GPUs.
-    for i in range(torch.cuda.device_count()):
-        major, minor = torch.cuda.get_device_capability(i)
-        cc = major * 10 + minor
-
-        if cc < 75:
-            raise RuntimeError(
-                "GPUs with compute capability less than 7.5 are not supported."
-            )
-
-    # figure out compute capability
-    compute_capabilities = {75, 80, 86, 89, 90}
-
     capability_flags = []
-    for cap in compute_capabilities:
-        capability_flags += ["-gencode", f"arch=compute_{cap},code=sm_{cap}"]
+
+    if CUDA_VERSION:
+        # Collect the compute capabilities of all available CUDA GPUs
+        for i in range(torch.cuda.device_count()):
+            major, minor = torch.cuda.get_device_capability(i)
+            cc = major * 10 + minor
+            if cc < 75:
+                raise RuntimeError(
+                    "GPUs with compute capability less than 7.5 are not supported."
+                )
+
+        # Figure out compute capability
+        compute_capabilities = {75, 80, 86, 89, 90}
+        for cap in compute_capabilities:
+            capability_flags += ["-gencode", f"arch=compute_{cap},code=sm_{cap}"]
 
     return capability_flags
 
 
-check_dependencies()
+def get_extra_compile_args():
+    extra_compile_args = {}
+
+    if os.name == "nt" and CUDA_VERSION:
+        include_arch = os.getenv("INCLUDE_ARCH", "1") == "1"
+
+        # Relaxed args on Windows
+        if include_arch:
+            extra_compile_args = {"nvcc": arch_flags}
+
+        cuda_path = os.environ.get("CUDA_PATH", None)
+        extra_link_args = ["-L", f"{cuda_path}/lib/x64/cublas.lib"]
+    elif CUDA_VERSION:
+        extra_compile_args = {
+            "cxx": ["-g", "-O3", "-fopenmp", "-lgomp", "-std=c++17", "-DENABLE_BF16"],
+            "nvcc": [
+                "-O3",
+                "-std=c++17",
+                "-DENABLE_BF16",
+                "-U__CUDA_NO_HALF_OPERATORS__",
+                "-U__CUDA_NO_HALF_CONVERSIONS__",
+                "-U__CUDA_NO_BFLOAT16_OPERATORS__",
+                "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+                "-U__CUDA_NO_BFLOAT162_OPERATORS__",
+                "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
+                "--expt-relaxed-constexpr",
+                "--expt-extended-lambda",
+                "--use_fast_math",
+            ]
+            + arch_flags
+            + generator_flags,
+        }
+
+    return extra_compile_args
+
+
+extensions = []
 extra_link_args = []
 include_dirs = get_include_dirs()
 generator_flags = get_generator_flag()
 arch_flags = get_compute_capabilities()
+extra_compile_args = get_extra_compile_args()
 
-if os.name == "nt":
-    include_arch = os.getenv("INCLUDE_ARCH", "1") == "1"
 
-    # Relaxed args on Windows
-    if include_arch:
-        extra_compile_args = {"nvcc": arch_flags}
-    else:
-        extra_compile_args = {}
-    
-    cuda_path = os.environ.get("CUDA_PATH", None)
-    extra_link_args = ["-L", f"{cuda_path}/lib/x64/cublas.lib"]
-else:
-    extra_compile_args = {
-        "cxx": ["-g", "-O3", "-fopenmp", "-lgomp", "-std=c++17", "-DENABLE_BF16"],
-        "nvcc": [
-            "-O3",
-            "-std=c++17",
-            "-DENABLE_BF16",
-            "-U__CUDA_NO_HALF_OPERATORS__",
-            "-U__CUDA_NO_HALF_CONVERSIONS__",
-            "-U__CUDA_NO_BFLOAT16_OPERATORS__",
-            "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
-            "-U__CUDA_NO_BFLOAT162_OPERATORS__",
-            "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
-            "--expt-relaxed-constexpr",
-            "--expt-extended-lambda",
-            "--use_fast_math",
-        ]
-        + arch_flags
-        + generator_flags,
-    }
-
-extensions = [
-    CUDAExtension(
-        "awq_ext",
-        [
-            "awq_ext/pybind_awq.cpp",
-            "awq_ext/quantization/gemm_cuda_gen.cu",
-            "awq_ext/layernorm/layernorm.cu",
-            "awq_ext/position_embedding/pos_encoding_kernels.cu",
-            "awq_ext/quantization/gemv_cuda.cu",
-        ],
-        extra_compile_args=extra_compile_args,
+if not ROCM_VERSION:
+    extensions.append(
+        CUDAExtension(
+            "awq_ext",
+            [
+                "awq_ext/pybind_awq.cpp",
+                "awq_ext/quantization/gemm_cuda_gen.cu",
+                "awq_ext/layernorm/layernorm.cu",
+                "awq_ext/position_embedding/pos_encoding_kernels.cu",
+                "awq_ext/quantization/gemv_cuda.cu",
+            ],
+            extra_compile_args=extra_compile_args,
+        )
     )
-]
 
 extensions.append(
     CUDAExtension(
@@ -183,8 +196,7 @@ extensions.append(
     )
 )
 
-
-if os.name != "nt":
+if os.name != "nt" and not ROCM_VERSION:
     extensions.append(
         CUDAExtension(
             "awq_ft_ext",
