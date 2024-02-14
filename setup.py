fa58402636abb808ed9358d33f4b589fbfa8ebca
Casper <casperbh.96@gmail.com>
2024-02-14 18:04:30 +0100

    MoE grouped gemm and fused topk_softmax (#8)
    * Initial
    
    * group gemm
    
    * Fix install. Add topk_softmax kernels.

--- setup.py
+++ setup.py
@@ -172,6 +172,9 @@ if CUDA_VERSION:
                 "awq_ext/layernorm/layernorm.cu",
                 "awq_ext/position_embedding/pos_encoding_kernels.cu",
                 "awq_ext/quantization/gemv_cuda.cu",
+                "awq_ext/vllm/moe_alig_block.cu",
+                "awq_ext/vllm/activation.cu",
+                "awq_ext/vllm/topk_softmax_kernels.cu",
             ],
             extra_compile_args=extra_compile_args,
         )
